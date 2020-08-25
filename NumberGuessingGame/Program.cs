10cef62c1a7d838cad6131eba90ce9a5e45d6746
Kelly <kelly.hoesing@gmail.com>
2020-08-25 11:22:45 -0500

    Initial state of NumberGuessing program

---
+++ NumberGuessingGame/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NumberGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"Too Low!");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine($"Too High!");
+            }
+            else
+                Console.WriteLine($"Nevermind");
+        }
+    }
+}
